bdc0d83229cb16fcfdb9032bad54057b6e57f6d3
cola594223 <76999614+cola594223@users.noreply.github.com>
2025-06-12 15:58:12 +0800

    嘗試啟用iis

--- app.js
+++ app.js
@@ -230,7 +230,7 @@ submitOrderButton.addEventListener('click', async () => {
     };
 
     try {
-        const response = await fetch('http://36.232.200.208:3000/api/orders', {
+        const response = await fetch('https://36.232.200.208:3000/api/orders', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
