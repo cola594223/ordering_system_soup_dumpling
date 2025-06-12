5e07d6a294dec4b50df74ae69f4424c45edc52a2
cola594223 <76999614+cola594223@users.noreply.github.com>
2025-06-13 02:13:34 +0800

    [修正] 更改外網網址

--- app.js
+++ app.js
@@ -258,7 +258,7 @@ submitOrderButton.addEventListener('click', async () => {
 
     try {
         // 發送訂單到後端 API
-        const response = await fetch('http://localhost:3000/api/orders', {
+        const response = await fetch('https://da00-2001-b011-b807-7b27-d0e7-5c4d-5ba6-f8ad.ngrok-free.app/api/orders', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
