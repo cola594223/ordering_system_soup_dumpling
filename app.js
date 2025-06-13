68eb6314e4fd8e7584276391314123d716e0c046
cola594223 <76999614+cola594223@users.noreply.github.com>
2025-06-13 13:38:03 +0800

    更新網址

--- app.js
+++ app.js
@@ -258,7 +258,7 @@ submitOrderButton.addEventListener('click', async () => {
 
     try {
         // 發送訂單到後端 API
-        const response = await fetch('https://ceec-2001-b011-b807-5a4e-a504-7afb-b589-81dd.ngrok-free.app/api/orders', {
+        const response = await fetch('https://4962-2001-b011-b807-5a4e-a504-7afb-b589-81dd.ngrok-free.app/api/orders', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
